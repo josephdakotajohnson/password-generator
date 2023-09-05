4cfc40d3e9f1e59371a6995bc2bee61b06e1fbbb
josephdakotajohnson <josephdakotajohnson@gmail.com>
2023-09-05 17:45:42 -0500

    Finished the characters included code

--- script.js
+++ script.js
@@ -5,47 +5,56 @@ var uppercaseArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 
 var numericArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var specialCharactersArr = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '|', ';', ':', '<', '>', ',', '.', '?'];
 
-// Write password to the #password input
-function writePassword() {
-  // var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+function generatePassword(){
   var passwordLength = window.prompt("What should the length of your password be?  (Must be between 8 and 128 characters.)");
   console.log(passwordLength);
-  if (passwordLength <= 7) {
-    window.alert("The password length must be above 7 characters!");
-  } else if (passwordLength >= 129) {
-    window.alert("The password length must be below 129 characters!");
+  if(passwordLength <=7 || passwordLength >= 129 || isNaN(passwordLength)) {
+    window.alert("The password length must be a number that is above 7 characters and below 129 characters!");
+    return "Invalid Length"
   }
-  var lowercaseIncluded = window.prompt("Should lowercase letters be included?  ('y' for yes or 'n' for no.)");
-  if ((lowercaseIncluded !== "y") && (lowercaseIncluded !== "n")) {
-    window.alert("Only 'y' or 'n' please!");
-    return;
-  } else {
-    console.log(lowercaseIncluded);
+  var charactersIncluded = []
+  var lowercaseIncluded = window.confirm("Should lowercase letters be included?  ('OK' for yes or 'Cancel' for no.)");
+//add lowercase characters to characters included variable
+//if they confirmed, then join
+ if (lowercaseIncluded === true) {
+  charactersIncluded = charactersIncluded.concat(lowercaseArr)
+ }
+  var uppercaseIncluded = window.confirm("Should uppercase letters be included?  ('OK' for yes or 'Cancel' for no.)");
+  if (uppercaseIncluded === true) {
+    charactersIncluded = charactersIncluded.concat(uppercaseArr)
   }
-  var uppercaseIncluded = window.prompt("Should uppercase letters be included?  ('y' for yes or 'n' for no.)");
-  if ((uppercaseIncluded !== "y") && (uppercaseIncluded !== "n")) {
-    window.alert("Only 'y' or 'n' please!");
-    return;
-  } else {
-    console.log(uppercaseIncluded);
+  var numericIncluded = window.confirm("Should numeric characters be included?  ('OK' for yes or 'Cancel' for no.)");
+  if (numericIncluded === true) {
+    charactersIncluded = charactersIncluded.concat(numericArr)
   }
-  var numericIncluded = window.prompt("Should numeric characters be included?  ('y' for yes or 'n' for no.)");
-  if ((numericIncluded !== "y") && (numericIncluded !== "n")) {
-    window.alert("Only 'y' or 'n' please!");
-    return;
-  } else {
-    console.log(numericIncluded);
+  var specialCharactersIncluded = window.confirm("Should special characters be included?  ('OK' for yes or 'Cancel' for no.)");
+  if (specialCharactersIncluded === true) {
+    charactersIncluded = charactersIncluded.concat(specialCharactersArr)
   }
-  var specialCharactersIncluded = window.prompt("Should special characters be included?  ('y' for yes or 'n' for no.)");
-  if ((specialCharactersIncluded !== "y") && (specialCharactersIncluded !== "n")) {
-    window.alert("Only 'y' or 'n' please!");
-    return;
-  } else {
-    console.log(specialCharactersIncluded);
+console.log(charactersIncluded)
+  if ((lowercaseIncluded === false) && (uppercaseIncluded === false) && (numericIncluded === false) && (specialCharactersIncluded === false)) {
+    window.alert("The password must have some characters in it!");
+    return "Invalid Characters"
   }
+  console.log(charactersIncluded)
+  var finalPassword = ""
+
+  for(let i=0; i< passwordLength; i++){
+   finalPassword +=charactersIncluded[(Math.floor(Math.random() * charactersIncluded.length))]
+  }
+ return finalPassword
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+//what kind of question is it?
